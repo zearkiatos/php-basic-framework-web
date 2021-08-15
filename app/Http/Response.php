db5ffdcc3ced09c6d0b799e050faa1df9d331838
zearkiatos <caprilespe@outlook.com>
2021-08-15 15:53:04 -0400

    Response

---
+++ app/Http/Response.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http;
+
+class Response
+{
+    protected $view;
+
+    public function __construct($view)
+    {
+        $this->view = $view;
+    }
+
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    public function send()
+    {
+        $view = $this->getView();
+
+        $content = file_get_contents(__DIR__ . "/../../views/$view.php");
+
+        require __DIR__ . "/../../views/layout.php";
+    }
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 It is a PHP 🐘 project to create a project with a commons frameworks structure
 
 ![Github](https://github.com/zearkiatos/php-basic-framework-web/actions/workflows/action.yml/badge.svg)
+![GitlabCI](https://gitlab.com/caprilespe/php-basic-framework-web/badges/develop/pipeline.svg)
