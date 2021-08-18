9c63d83a9654071fb5a2a0b425a221df3e2aa6dd
zearkiatos <caprilespe@outlook.com>
2021-08-17 20:25:53 -0400

    Views

---
+++ views/layout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Web Site</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div class="container">
+            <a href="/" class="navbar-brand h1">FW 🐘</a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <?php echo $content ?>
+        </div>
+    </div>
+</body>
+
+</html>
