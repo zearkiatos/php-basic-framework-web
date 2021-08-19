ff504b4b4781f0681ec0fc88f40f14a8936bf81e
zearkiatos <caprilespe@outlook.com>
2021-08-18 20:44:39 -0400

    Helpers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Response;
 
 class HomeController
 {
     public function index()
     {
-        return new Response('home');
+        return view('home');
     }
 }
--- app/Http/Response.php
+++ app/Http/Response.php
@@ -20,8 +20,8 @@ class Response
     {
         $view = $this->getView();
 
-        $content = file_get_contents(__DIR__ . "/../../views/$view.php");
+        $content = file_get_contents(viewPath($view));
 
-        require __DIR__ . "/../../views/layout.php";
+        require viewPath('layout');
     }
 }
